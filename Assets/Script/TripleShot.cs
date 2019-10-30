aa478ffa333342ad0eaaaf2578682a25561e847d
Marco Coccato <coccato.m@gmail.com>
2019-10-30 18:48:12 +0100

    Added TripleShot script to managed tripleShot gameobject destruction

---
+++ Assets/Script/TripleShot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleShot : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.childCount < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
