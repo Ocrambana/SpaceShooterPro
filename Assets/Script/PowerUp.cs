6587cbfaf93ba244af9a8d5c9086f5747a0ed744
Marco Coccato <coccato.m@gmail.com>
2019-11-02 12:27:53 +0100

    Refactore powerup script

--- Assets/Script/PowerUp.cs
+++ Assets/Script/PowerUp.cs
@@ -11,31 +11,41 @@ public class PowerUp : MonoBehaviour
 
     private void Start()
     {
-        float xComponent = Random.Range(-3f,3f);
-        float yComponent = Random.Range(-5f,-1f);
-        movementDirection = new Vector3(xComponent,yComponent,0f);
-        movementDirection.Normalize();
+        CalculateMovementDirection();
 
         Destroy(gameObject, 6f);
     }
 
+    private void CalculateMovementDirection()
+    {
+        float xComponent = Random.Range(-3f, 3f);
+        float yComponent = Random.Range(-5f, -1f);
+        movementDirection = new Vector3(xComponent, yComponent, 0f);
+        movementDirection.Normalize();
+    }
+
     void Update()
+    {
+        CalculateMovement();
+
+        if (transform.position.y < -5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CalculateMovement()
     {
         Vector3 movement = movementDirection * _speed * Time.deltaTime;
         float newX = transform.position.x + movement.x;
 
-        if(newX < -9f || newX > 9f)
+        if (newX < -9f || newX > 9f)
         {
             movementDirection = Vector3.Reflect(movementDirection, Vector3.right);
             movement = Vector3.Reflect(movement, Vector3.right);
         }
 
         transform.Translate(movement);
-
-        if (transform.position.y < -5f)
-        {
-            Destroy(gameObject);
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
