7bc82934bdee74f76e23485d9261cf490471497a
Marco Coccato <coccato.m@gmail.com>
2019-11-04 17:29:47 +0100

    Added ui showing lives

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI _scoreText;
+    [SerializeField]
+    private Image _livesImage;
+    [SerializeField]
+    private List<Sprite> _liveSprites;
 
     void Start()
     {
-        _scoreText.text = "0";   
+        UpdateLives(_liveSprites.Count);
+        UpdateScore(0);
     }
 
     public void UpdateScore(int val)
     {
         _scoreText.text = val.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImage.sprite = _liveSprites[currentLives];
     }
 }
